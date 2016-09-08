f8bdb9486c17c237c04b60a5796dfe7243ea8af9
Sovereign <git+sovereign@minepass.net>
2016-09-07 19:53:39 -0700

    Phrasing.

--- src/main/java/net/minepass/gs/mc/wrapper/ScheduledTasks.java
+++ src/main/java/net/minepass/gs/mc/wrapper/ScheduledTasks.java
@@ -91,7 +91,7 @@ public class ScheduledTasks extends GameserverTasks implements Runnable {
         wrapper.getServerManager().tellPlayerRaw(playerId.toString(),
                 String.format(
                         "[\"\",{\"text\":\"%s\",\"color\":\"aqua\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"%s\"}}]",
-                        message.concat(" Click for your World Pass."),
+                        message.concat(" Click to get your MinePass."),
                         wrapper.getMinepass().getServer().join_url
                 )
         );
